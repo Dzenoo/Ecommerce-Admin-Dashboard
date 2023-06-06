e0b4708c24cab96b7254a8ab7c77ba6c202acd42
Dzenis Gudzevic <frontprogramer@gmail.com>
2023-06-06 20:42:16 +0200

    Update Users.js

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -5,7 +5,8 @@ const Users = () => {
   const main = useContext(MainContext);
   return (
     <div className="user_wrapper">
-      <UserItem user={main.users} />
+    
+       <UserItem user={main.users} />
     </div>
   );
 };
